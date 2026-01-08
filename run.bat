b584523a091e62028bb643d64913c289a355a65b
k17ze <k17ze@users.noreply.github.com>
2026-01-08 17:03:36 +0530

    Clean repository: Remove build files and keep only source code

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running NHS Healthcare Management System...
-java -cp build\classes nhs.NHSApplication
-pause
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Run script for NHS Healthcare Management System
-
-echo "Running NHS Healthcare Management System..."
-java -cp build/classes nhs.NHSApplication
